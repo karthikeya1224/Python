e82f0a2fbc1fb3dc865e57bf1302acd6368a81d7
karthikeya1224 <karthikeyakadali2000@gmail.com>
2025-02-28 11:47:46 +0530

    2nd commit

--- Regular Expression/file23.py
+++ Regular Expression/file23.py
@@ -1,8 +1,6 @@
 import re
-
 email = input('enter email:').strip().lower()
 pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-
 if re.match(pattern, email):
     print("Valid email")
 else:
